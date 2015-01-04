e92567dc807f8ddffd69c4745a7a95e3cda60ee5
tbk <theborakompanioni@gmail.com>
2015-01-04 23:41:52 +0100

    removed docular docs

--- docs/documentation/docular-configuration.js
+++
@@ -1,4 +0,0 @@
-baseURL = '/'; useHtml5Mode=false; window.discussionConfigs = {active:false, shortName:'false', url:'false', dev:false}; autoloadExamples = true;
-exampleJs = [];
-exampleCss = [];
-loadAngular = true;
--- docs/documentation/groups-metadata.js
+++
@@ -1,3 +0,0 @@
-GROUP_DATA=[
-{"groupTitle":"VisSense","groupId":"vissense","groupIcon":"icon-beer","showSource":true,"sections":[
-{"id":"api","title":"APIs","scripts":["lib/vissense.js"],"doc_api":"doc"}],"visible":true}];
